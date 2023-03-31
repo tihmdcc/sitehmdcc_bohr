cc311d0990e8178aec21ddf73f32e6af782b7922
tihmdcc <contato.hmdcc@gmail.com>
2023-03-31 14:15:47 -0300

    output standalone

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  compress: false,
 }
 
 module.exports = nextConfig
